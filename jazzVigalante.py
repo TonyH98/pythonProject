3dcf703bda15707a25586fc5f2ef3644cc9031ec
TonyH98 <tonyhoang@pursuit.org>
2024-12-29 13:36:46 -0500

    Created Game State variables, and finished the menu method

--- jazzVigalante.py
+++ jazzVigalante.py
@@ -4,3 +4,8 @@ class JazzVigalante(monster):
 
     def __init__(self, health, strength, defense, speed, intelligence, mp, skills, exp, money):
         super().__init__(health, strength, defense, speed, intelligence, mp, skills, exp, money)
+    
+
+    def vigalanteAI(self, player):
+        if(player.health < player.health / 2):
+            self.defense 
